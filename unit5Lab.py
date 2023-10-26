fc42e239cf43dead0db2ca282a2942b89e0ffa99
DylanLiesenfelt <dylanjames.code@gmail.com>
2023-10-26 02:02:13 -0400

    Update unit5Lab.py
    comments

--- unit5Lab.py
+++ unit5Lab.py
@@ -25,74 +25,63 @@
 # Note
 # Define the number of rounds per game as a constant so it can easily be adjusted to make the game more or less challenging.
 
+import random #Imports the random libary
 
-# def chancesCounter(chancesLeft, wasUserCorrect):
-    #     if wasUserCorrect == False and chancesLeft > 0:
-    #         chancesLeft -= 1
-    #         return chancesLeft
-    #     elif wasUserCorrect == False and chancesLeft == 0:
-    #         return chancesLeft 
-    #     else:
-    #         return chancesLeft
-
-import random
-
-def main():
+def main(): # Defines the main function of this program
 
     # Functions
-    
-    def getGuess(firstNum, lastNum, chances):
+    def getGuess(firstNum, lastNum, chances): # Defines getGuess function, function takes user input and returns that value to the function
         print(f'\nChances left: {chances}')
-        userGuess = int(input(f'Please enter a whole number {firstNum} and {lastNum}: '))
-        while userGuess < firstNum or userGuess > lastNum:
+        userGuess = int(input(f'Please enter a whole number {firstNum} and {lastNum}: ')) # Assigns the users input to the variable userGuess
+        while userGuess < firstNum or userGuess > lastNum: #Validation Loop
             print(f'ERROR: You entered {userGuess}. \nThat is not a whole number between {firstNum} and {lastNum}.')
             userGuess = int(input(f'\nPlease enter a whole number {firstNum} and {lastNum}: '))
-        else:
+        else: # If users input is within the specfied range assigned to the parameters firstNum and LastNums, then userGuess is returned to the function
             return userGuess
         
-    def guessWin(guess, number):
-        if guess > number:
+    def guessWin(guess, number): # Defines the guessWin function, determines if the user input is higher, lower, or equals the value of the random number
+        if guess > number: # Checks if user input is higher than the random number, Returns value of False to the function
             print('Too high, try again.')
             return False
-        elif guess < number:
+        elif guess < number: # Checks if user input is lower than the random number, Returns value of False to the function
             print('Too low, try again.')
             return False
-        else:
+        else: # If users input is not higher or lower than the random number it must equal the random number. Returns the value of True to the function
             return True
 
-    def playAgain():
+    def playAgain(): # Defines playAgain function, takes user input and returns the string's value to the function 
         playAgain = input(f'To play again enter y, to exit hit ENTER: ')
         return playAgain
     
-    def game(first, last, chances):
-        number = random.randint(first, last)
-        guessWasCorrect = False
-        chances
+    def game(first, last, chances): # Defines the game function, plays the game using the previous functions. 
+        number = random.randint(first, last) # Assigns a random integer btween the values of the parameters fist and last to the variable number
+        guessWasCorrect = False # Assigns a default value of false to the variable guessWasCorrect 
+        chances # Brings the value of chances parameter into the local scope of the function
 
-        while chances > 0 and guessWasCorrect == False:
-            if guessWasCorrect == False:
-                    guess = getGuess(first, last, chances)
-                    guessWasCorrect = guessWin(guess, number)
-                    chances -= 1
-                    if guessWasCorrect == True:
+        while chances > 0: # Checks if chances are greater than zero per iteration of loop
+            if guessWasCorrect == False: # If guessWasCorrect still equals False loop proceeds
+                    guess = getGuess(first, last, chances) # Calls the getGuess function and assigns its value to variable guess
+                    guessWasCorrect = guessWin(guess, number) # Calls guessWin variable passes the value of guess as parameter along with the value of the random number, the returned bool value rewrites the value of the variable wasGuessCorrect
+                    chances -= 1 # Decreases the value of chances by per iteration of the loop
+                    if guessWasCorrect == True: # If during the loop guessWasCorrect bool value was updated to equal true prints congrats message and breaks out of loop. 
                         print(f'\nCongrats {number} was the answer! You win!')
                         break
-        else: 
+        else: # If chances decrease to 0 loop ends and loss message is displayed
             print('\nYou ran out of chances :( You lose.')
     
-        playStatus = playAgain()
+        playStatus = playAgain() # After loop ends calls playAgain function and assigns its returned value to playStatus and returns its value to the game function 
         return playStatus       
 
     # Global Variables
-    FIRST_NUMBER = 1
-    LAST_NUMBER = 100
-    CHANCES = 7
-    playStaus = 'y'
+    FIRST_NUMBER = 1 # This number will used as the first value in the range for determining the random number added as const for later change for increased or decreased difficulty
+    LAST_NUMBER = 100 # Same as FIRST_NUMBER but last number in the range.
+    CHANCES = 7 # This number will be used to detremine how many chances you have in the game, added as const for same reason as FIRST_NUMBER
+    playStaus = 'y' # Default value of the playStaus will be used to see if user wants to play the game
 
-    print('Welcome to my number guessing game!')
+    print('Welcome to my number guessing game!') 
 
-    while playStaus == 'y' or playStaus == 'Y':
-        playStaus = game(FIRST_NUMBER, LAST_NUMBER, CHANCES)
-    else: print('Good Bye!') 
+    while playStaus == 'y' or playStaus == 'Y': # Checks if play status equals y or Y, if so loop iterates  
+        playStaus = game(FIRST_NUMBER, LAST_NUMBER, CHANCES) # Calls the game function, passes global consts as parameters for the functions, when game function completes and its value is returned as the value of the playAgain function that value  will update the playStaus variable 
+    else: print('Good Bye!') # If playStatus does not equal y or Y goodbye message prints and program exits 
 
 main()
