85f0465f666521792d4fee1904678b350eeef625
DylanLiesenfelt <dylanjames.code@gmail.com>
2023-11-30 13:50:16 -0500

    final done

--- final.py
+++ final.py
@@ -10,57 +10,24 @@ def menuNav():
     return int(menu)
 
 def menu1():
-    print('\nVingt-et-un is a dice game (known as Blackjack in the USA), where a player competes against the computer (house).\n',
-        'The goal of the game is to score 21 points, or as near as possible without going over.',
+    print('\n Vingt-et-un is a dice game (known as Blackjack in the USA), where a player competes against the computer (house).\n',
+        'The goal of the game is to score 21 points, or as near as possible without going over.\n',
         'The two players take turns throwing two dies as many times as desired and adding up the numbers thrown on each round.\n',
-        '\nA player who totals over 21 is bust and loses the game.\n',
+        '\n A player who totals over 21 is bust and loses the game.\n',
         'The player whose total is nearest 21, after each player has had a turn, wins the game.\n',
         'In the case of an equality high total, the game is tied.\n',
-        '\nThe game is over at the end of a round when:\n',
+        '\n The game is over at the end of a round when:\n',
         'One or both players are bust.\n',
         'Both players choose to stay.\n',
-        '\nOnce a player totals 14 or more, one of the dies is discarder for the consecutive turns.\n',
+        '\n Once a player totals 14 or more, one of the dies is discarder for the consecutive turns.\n',
         'The house must throw the dice until the total is 17 or higher. At 17 or higher, the house must stay.')
 
 def displayScore(house, name, player):
     print(f'\nHouse: {house}  ||  {name}: {player}')
     print('-' * 25)
 
-def houseTurn(houseScore):
-    if houseScore >= 17:
-        print('House Stays')
-        return 0
-    else:
-        if houseScore > 14:
-            roll1 = diceRoll()
-            print(f'House Roll: {roll1}')
-            return roll1
-        else:
-            roll1 = diceRoll()
-            roll2 = diceRoll()
-            print(f'House Roll: {roll1} & {roll2}')
-            roll = roll1 + roll2
-            return roll
 
-def playerTurn(playerChoice, playerName, playerScore):
-    if playerChoice == 'h' or playerChoice == 'H':
-        if playerScore > 14:
-            roll1 = diceRoll()
-            print('-' * 25)
-            print(f'\n{playerName} Roll: {roll1}')
-            return roll1
-        else:
-            roll1 = diceRoll()
-            roll2 = diceRoll()
-            print('-' * 25)
-            print(f'\n{playerName} Roll: {roll1} & {roll2}')
-            roll = roll1 + roll2
-            return roll
-    if playerChoice == 's' or playerChoice == 'S':
-        print(f'{playerName} Stays')
-        return 0
-
-def gameOver(playerScore, playerName, houseScore):
+def gameOver(playerScore, playerName, houseScore, houseStay, playerStay):
     if playerScore >= 21 or houseScore >= 21:
         if playerScore == 21 and houseScore != 21:
             print(f'{playerName} Wins: {playerScore}')
@@ -72,10 +39,39 @@ def gameOver(playerScore, playerName, houseScore):
             print(f'House Bust {houseScore}')
         elif playerScore == 21 and houseScore == 21:
             print(f'Both {playerName} and House have 21, Draw.')
+        elif playerScore > 21 and houseScore > 21:
+            print(f'House and {playerName} both bust, Draw')
+    elif houseStay == True and playerStay == True:
+        if playerScore > houseScore:
+            print(f'{playerName} Wins {playerScore}, House Loses {houseScore}')
+        elif playerScore < houseScore:
+            print(f'House Wins {houseScore}, {playerName} Loses {playerScore}')
 
 
+def playerTurn(playerName, playerScore):
+    if playerScore >= 14:
+        roll = diceRoll()
+        print(f'\n     {playerName} Roll: {roll}')
+        return roll
+    else:
+        roll1 = diceRoll()
+        roll2 = diceRoll()
+        print(f'\n     {playerName} Roll: {roll1} & {roll2}')
+        return roll1 + roll2
 
-
+def houseTurn(houseScore):
+    if houseScore >= 17:
+        print('House Stays')
+        return 0
+    elif houseScore >= 14:
+        roll = diceRoll()
+        print(f'\n     House Roll: {roll}')
+        return roll
+    else:
+        roll1 = diceRoll()
+        roll2 = diceRoll()
+        print(f'\n     House Roll: {roll1} & {roll2}')
+        return roll1 + roll2
 
 def menu2(playerName):
     houseScore = 0
@@ -83,25 +79,38 @@ def menu2(playerName):
     print('\nGame Start')
     displayScore(houseScore, playerName, playerScore)
     
-    while playerScore <= 21 and houseScore <= 21:
-            
-        playerChoice = input('ENTER h to HIT, or s to STAY:')
-        while playerChoice not in ['h','H','s','S']:
+    playerStay = False
+    houseStay = False
 
-            print('ERROR: You did not ENTER h or s.')
-            print(playerChoice)
-            playerChoice = input('ENTER h to HIT, or s to STAY:', '\n')
-
-        else: 
-            playerResult = playerTurn(playerChoice, playerName, playerScore)
+    while not (playerStay and houseStay):
+        if not playerStay:
+            playerChoice = input('ENTER h to HIT, or s to STAY: ').lower()
+            while playerChoice != 'h' and playerChoice != 's':
+                print('ERROR: you did not enter h or s.\n')
+                print(playerChoice)
+                playerChoice = input('ENTER h to HIT, or s to STAY: ').lower()
+            else:
+                if playerChoice == 'h':
+                    playerResult = playerTurn(playerName, playerScore)
+                    playerScore += playerResult
+                    displayScore(houseScore, playerName, playerScore)
+                if playerScore >= 21:
+                    playerStay = True
+                elif playerChoice == 's':
+                    print(f'{playerName} Stays')
+                    playerStay = True
+        if not houseStay:
             houseResult = houseTurn(houseScore)
-            playerScore += playerResult
             houseScore += houseResult
             displayScore(houseScore, playerName, playerScore)
-    else:
-        gameOver(playerScore, playerName, houseScore)
-        playAgain = input('Would you like to play again (y/n): ')
-        return playAgain
+            if houseScore >= 21:
+                houseStay = True
+            elif houseScore >= 17:
+                houseStay = True
+
+    gameOver(playerScore, playerName, houseScore, playerStay, houseStay)
+    playAgain = input('\nWould you like to play again (y/n): ')
+    return playAgain
 
 
 def main():
